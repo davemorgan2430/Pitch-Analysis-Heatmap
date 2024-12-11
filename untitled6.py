c5848f6dc914da8ac810633c5c262b4c41b6e3e8
davemorgan2430 <davemorgan2430@yahoo.com>
2024-12-11 16:27:46 -0500

    Update untitled6.py

--- untitled6.py
+++ untitled6.py
@@ -262,9 +262,8 @@ import plotly.express as px
 st.header("Pitcher Movement vs. League Average Heatmap")
 
 import plotly.express as px
-    pitcher_name = st.selectbox("Select a pitcher for comparison", df['player_name'].unique())
-    pitcher_df = df[df['player_name'] == pitcher_name]
-    
+pitcher_name = st.selectbox("Select a pitcher for comparison", df['player_name'].unique())
+pitcher_df = df[df['player_name'] == pitcher_name]
     if not pitcher_df.empty:
         league_data = df[df['p_throws'] == pitcher_df['p_throws'].iloc[0]]
         league_data_grouped = league_data.groupby(['pitch_type']).agg({'HB': 'mean', 'iVB': 'mean'}).reset_index()
